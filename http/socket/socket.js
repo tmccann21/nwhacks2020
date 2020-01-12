aa06a3bec76f735be0b42a113ed181a66b0148f0
Ceylin Ozdemir <ceylin@Ceylins-MacBook-Pro.local>
2020-01-11 18:27:44 -0800

    Google Cloud Stuff

--- http/socket/socket.js
+++ http/socket/socket.js
@@ -1,19 +1,53 @@
+const WebSocket = require("ws");
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const wss = new WebSocket.Server({ server });
+
+//Include Google Speech to Text
+const speech = require("@google-cloud/speech");
+const client = new speech.SpeechClient();
+
+//Configure Transcription Request
+const request = {
+  config: {
+    encoding: "MULAW",
+    sampleRateHertz: 8000,
+    languageCode: "en-GB"
+  },
+  interimResults: true
+};
+
 const socket = (ws) => {
   console.log('a socket has connected');
+
+  let recognizeStream = null;
+
   ws.on("message", function incoming(message) {
     const msg = JSON.parse(message);
     switch (msg.event) {
       case "connected":
         console.log(`A new call has connected.`);
+
+        // Create Stream to the Google Speech to Text API
+        recognizeStream = client
+          .streamingRecognize(request)
+          .on("error", console.error)
+          .on("data", data => {
+            console.log(data.results[0].alternatives[0].transcript);
+          });
+
         break;
       case "start":
         console.log(`Starting Media Stream ${msg.streamSid}`);
         break;
       case "media":
-        console.log(`Receiving Audio...`)
+        // Write Media Packets to the recognize stream
+        recognizeStream.write(msg.media.payload);
         break;
       case "stop":
         console.log(`Call Has Ended`);
+        recognizeStream.destroy();
         break;
     }
   });
